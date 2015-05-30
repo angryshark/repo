de5aaa830e42dea94d989f3a61ee79814780c1a7
Dzmitry Niadzelka <week62@gmail.com>
2015-05-30 16:45:08 +0300

    loads videos

--- youtube.js
+++ youtube.js
@@ -2,6 +2,8 @@
 var videoItems = document.querySelector('.videoItems');
 var clickSearch = document.querySelector('.submitSearch');
 var inputString = document.querySelector('.inputSearch');
+var pageToken = '';
+var searchString = '';
 
 clickSearch.onclick = function() {
 	doSearch();
@@ -21,18 +23,24 @@ function doSearch() {
 	if (inputString.value === '') {
 		return;
 	} else {
-        getResponse(inputString.value);
+        searchString = inputString.value;
+        getResponse(searchString, pageToken);
     }
 }
 
+function loadPages () {
+    getResponse(searchString, pageToken); 
+}
 
-function getResponse(searchString) {
+
+function getResponse(searchString, pageToken) {
     var APIkey = 'AIzaSyBtEdPEv4Diqrkyg9mRT2M-KjLfu_0qjCk';
-    var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + searchString + '&maxResults=20&key=' + APIkey;
+    var url = 'https://www.googleapis.com/youtube/v3/search?pageToken=' + pageToken + '&part=snippet&q=' + searchString + '&maxResults=20&key=' + APIkey;
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var clipList = convertYouTubeResponseToClipList(JSON.parse(xhr.responseText));
+           // videoItems.style.width = (numOfPageLoaded * 100) + '%' ;
             doInnerContent(clipList);
         }
     }
@@ -40,7 +48,11 @@ function getResponse(searchString) {
     xhr.send(null);
 }
 
+
 function doInnerContent (clipList) {
+    var loadBlock = document.createElement('div');
+    loadBlock.className = 'loadBlock';
+    videoItems.appendChild(loadBlock);
     for (var i = 0; i < 20; ++i) {
         var newDiv = document.createElement('div');
         newDiv.className = 'v' + (i+1) + ' ' + 'video';
@@ -48,14 +60,15 @@ function doInnerContent (clipList) {
                          '<div class='+'thumbnail'+' style='+'background-image:url('+clipList[i].thumbnail+')></div>'+
                          '<div class='+'author'+'>'+'<p>'+'<b>Author: </b>'+clipList[i].author+'</p>'+'</div>'+
                          '<div class='+'description'+'>'+'<p>'+'<b>Description: </b>'+clipList[i].description+'</p>'+'</div>'+
-                         '<div class='+'publishDate'+'>'+'<p>'+'<b>Publication date: </b><br>'+clipList[i].publishDate+'</p>'+'</div>';                   
-        videoItems.appendChild(newDiv);
-    }
+                         '<div class='+'publishDate'+'>'+'<p>'+'<b>Publication date: </b><br>'+clipList[i].publishDate+'</p>'+'</div>';
+        loadBlock.appendChild(newDiv);
+    }    
 }
 
 function convertYouTubeResponseToClipList(rawYouTubeData) {
 	var clipList = [];
     var items = rawYouTubeData.items;
+    pageToken = rawYouTubeData.nextPageToken;
     for (var i = 0; i < items.length; i++) {
     	var date = new Date(Date.parse(items[i].snippet.publishedAt));
         var shortId = items[i].id.videoId;
@@ -78,12 +91,12 @@ var diffX;
 var dragX;
 var startX;
 var currentPage = 1;
+var numOfPageLoaded = 5;
 
 videoItems.addEventListener('mousedown',dragStart);
 videoItems.addEventListener('mouseup',dragEnd);   
 
 function dragStart(e){
-    console.log('dragStart');
     this.style.transition="all 0.0s ease-in-out"
     startX = e.clientX;
     videoItems.addEventListener('mousemove',drag);
@@ -96,24 +109,26 @@ function drag(e){
 }
 
 function dragEnd(e){
-        console.log('dragEnd');
     videoItems.removeEventListener('mousemove',drag);   
     videoItems.style.transition = "all 0.5s ease-in-out 0s";
 
+    if (currentPage === numOfPageLoaded) {
+        numOfPageLoaded = numOfPageLoaded + 5;
+        loadPages();
+    }
+    
     if (Math.abs(diffX) < 100) {
         videoItems.style.webkitTransform = "translateX("+previousDiffX+"px)";
     }
 
     if (diffX < -100) {
-        if (currentPage !== 5) {
-            previousDiffX = previousDiffX - document.body.offsetWidth;
-            this.style.webkitTransform = "translateX(" + previousDiffX + "px)"; 
-            currentPage = currentPage + 1;
-        } else  videoItems.style.webkitTransform = "translateX("+previousDiffX+"px)";    
-    }
+        previousDiffX = previousDiffX - document.body.offsetWidth;
+        this.style.webkitTransform = "translateX("+previousDiffX+"px)"; 
+        currentPage = currentPage + 1;
+    } else videoItems.style.webkitTransform = "translateX("+previousDiffX+"px)";
 
     if (diffX > 100) {
-        if(currentPage !== 1) {
+        if (currentPage !==1) {
             previousDiffX = previousDiffX + document.body.offsetWidth;
             this.style.webkitTransform = "translateX("+previousDiffX+"px)";
             currentPage = currentPage - 1;
